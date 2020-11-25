ce11ce378ef6dcab3e0f2345d7022ed8580bc60c
A.Arimura <404ari@gmail.com>
2020-11-25 20:14:41 +0900

    🦾  initial commit 👏

--- config.js
+++ config.js
@@ -3,12 +3,12 @@
 module.exports = {
   url: 'https://lumen.netlify.com',
   pathPrefix: '/',
-  title: 'Blog by John Doe',
-  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
-  copyright: '© All rights reserved.',
+  title: 'メモモーモモ・モーモモ',
+  subtitle: 'モがゲシュタルト崩壊したと思います。',
+  copyright: '© wasshoy All rights reserved.',
   disqusShortname: '',
   postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
+  // googleAnalyticsId: 'UA-73379983-2',
   useKatex: false,
   menu: [
     {
@@ -25,15 +25,15 @@ module.exports = {
     }
   ],
   author: {
-    name: 'John Doe',
-    photo: '/photo.jpg',
-    bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+    name: 'wasshoy',
+    photo: '/breadclip.png',
+    bio: 'ノーマル/ノーマル タイプのエンジニアです。',
     contacts: {
       email: '',
       facebook: '#',
       telegram: '#',
       twitter: '#',
-      github: '#',
+      github: 'wasshoy',
       rss: '',
       vkontakte: '',
       linkedin: '#',
@@ -41,7 +41,7 @@ module.exports = {
       line: '',
       gitlab: '',
       weibo: '',
-      codepen: '',
+      codepen: 'wasshoy',
       youtube: '',
       soundcloud: '',
       medium: '',
---
+++ content/posts/2020-11-25---My-First-Post.md
@@ -0,0 +1,20 @@
+---
+title: 'Gatsbyでブログを作成'
+date: '2020-11-19'
+template: 'post'
+draft: false
+slug: 'my-first-post'
+category: 'Gatsby'
+tags:
+  - 'Gatsby'
+  - 'JavaScript'
+description: 'Gatsbyでブログを作成しました（このサイト）。公開までに踏んだ手順を書いていきます。'
+socialImage: '/media/42-line-bible.jpg'
+---
+
+Blazing Fast で静的サイトを作成できる[Gatsby](https://www.gatsbyjs.com/)を使って本ブログを作成しました。
+
+- [Gatsby のインストール](#Gatsby のインストール)
+- 内容がないよう。
+
+## Gatsby のインストール
